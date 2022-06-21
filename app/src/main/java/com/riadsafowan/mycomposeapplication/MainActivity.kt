2ae4967c2259385fb59b4aeb1d43b1faebcc0c27
Riad Safowan <riad.safowan@northsouth.edu>
2022-06-21 18:48:04 +0600

    Image, card, Box

--- app/src/main/java/com/riadsafowan/mycomposeapplication/MainActivity.kt
+++ app/src/main/java/com/riadsafowan/mycomposeapplication/MainActivity.kt
@@ -3,16 +3,27 @@ package com.riadsafowan.mycomposeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.riadsafowan.mycomposeapplication.ui.theme.MyComposeApplicationTheme
 import com.riadsafowan.mycomposeapplication.ui.theme.Shapes
 
@@ -21,39 +32,88 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DefaultPreview()
+            MyView()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(showBackground = true)
+private fun MyView() {
+    val painter = painterResource(id = R.drawable.ic_launcher_background)
+    Column() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ImageCard(painter, "adf", "Hello from Android")
+            ImageCard(painter, "adf", "another from Android")
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(0.dp, 10.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            ImageCard(painter, "adf", "Hello from Android")
+            ImageCard(painter, "adf", "another from Android")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MyComposeApplicationTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(color = Color.Red)
-                .padding(all = 50.dp)
-            ,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceBetween
+fun ImageCard(
+    painter: Painter,
+    contentDes: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp,
+
         ) {
-            Row (modifier = Modifier
-                .border(15.dp, Color.Black , shape = Shapes.medium)
-                .padding(25.dp))
-            {
-                Text(text = "Riad Safowan")
-                Spacer(modifier = Modifier.width(20.dp))
-                Text(text = "Safowan Riad")
+        Box(
+            modifier = Modifier
+                .height(300.dp)
+                .width(200.dp)
+                .background(Color.Green)
+        ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painter,
+                contentDescription = contentDes,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 500f,
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
             }
-            Greeting("Riwertwretbrtwer  werg ergread")
-            Greeting(name = "safodfvbwan")
+
+
         }
+
     }
 }
