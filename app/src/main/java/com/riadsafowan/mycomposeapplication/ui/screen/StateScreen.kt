5094935c48a4eadde2d08ff87c5b72d644d58b02
Riad Safowan <riad.safowan@northsouth.edu>
2022-07-01 09:03:58 +0600

    State

--- app/src/main/java/com/riadsafowan/mycomposeapplication/ui/screen/StateScreen.kt
+++ app/src/main/java/com/riadsafowan/mycomposeapplication/ui/screen/StateScreen.kt
@@ -1,10 +1,14 @@
 package com.riadsafowan.mycomposeapplication.ui.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -17,6 +21,7 @@ import androidx.navigation.compose.rememberNavController
 import com.riadsafowan.mycomposeapplication.Screen
 import com.riadsafowan.mycomposeapplication.fontFamily
 import com.riadsafowan.mycomposeapplication.ui.components.NavBar
+import kotlin.random.Random
 
 @Composable
 fun StateScreen(navController: NavController) {
@@ -29,28 +34,21 @@ fun StateScreen(navController: NavController) {
             navController = navController,
             previous = Screen.TextScreen.route
         )
-        Row {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(IntrinsicSize.Min)
-                    .background(Color(0xFF000000))
-            ) {
-                Column() {
-                    Text(
-                        text = "statefull compose",
-                        color = Color.White,
-                        fontSize = 30.sp,
-                        fontFamily = fontFamily
-                    )
-                    Text(
-                        text = "hello world",
-                        style = TextStyle(color = Color.Blue)
-                    )
-                }
-            }
-        }
+        ColorChangingBox()
+
     }
+}
+
+@Composable
+fun ColorChangingBox() {
+    val color = remember { mutableStateOf(Color.Red) }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = color.value)
+        .clickable {
+            color.value = Color(Random.nextFloat(), Random.nextFloat(), Random.nextFloat(), 1f)
+        })
 }
 
 @Composable
